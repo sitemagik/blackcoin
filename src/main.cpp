33c1e048bc9c50f65af7ea9b4be1ae4741659768
sitemagik <sitemagik@gmail.com>
2016-12-09 14:50:30 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1999,7 +1999,7 @@ bool LoadBlockIndex(bool fAllowNew)
         //   vMerkleTree: 97ddfbbae6
 
         // Genesis block
-        const char* pszTimestamp = "NY Times 05/Oct/2011 Steve Jobs, Apple’s Visionary, Dies at 56";
+        const char* pszTimestamp = "NY Times 05/Dec/2016 Donald Trump, Is a fucking moron";
         CTransaction txNew;
         txNew.vin.resize(1);
         txNew.vout.resize(1);
@@ -2011,13 +2011,13 @@ bool LoadBlockIndex(bool fAllowNew)
         block.hashPrevBlock = 0;
         block.hashMerkleRoot = block.BuildMerkleTree();
         block.nVersion = 1;
-        block.nTime    = 1371611186;
+        block.nTime    = 1481323532;
         block.nBits    = 0x1e0ffff0;
         block.nNonce   = 385948866;
 
         if (fTestNet)
         {
-            block.nTime    = 1371611186;
+            block.nTime    = 1481323532;
             block.nNonce   = 385948866;
         }
 
