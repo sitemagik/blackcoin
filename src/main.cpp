e7dba4c8218d0f9f5166c66b7deb652880bcde33
sitemagik <sitemagik@gmail.com>
2016-12-11 01:53:23 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -30,7 +30,7 @@ CTxMemPool mempool;
 unsigned int nTransactionsUpdated = 0;
 
 map<uint256, CBlockIndex*> mapBlockIndex;
-uint256 hashGenesisBlock("371d845655c9279a5e3c4a2dd43fbe02ffc8bb34c8bde65a33d818209c72022b");
+uint256 hashGenesisBlock("oxo");
 static CBigNum bnProofOfWorkLimit(~uint256(0) >> 20); // sweet: starting difficulty is 1 / 2^12
 CBlockIndex* pindexGenesisBlock = NULL;
 int nBestHeight = -1;
@@ -1972,7 +1972,7 @@ bool LoadBlockIndex(bool fAllowNew)
         pchMessageStart[1] = 0xc5;
         pchMessageStart[2] = 0xbc;
         pchMessageStart[3] = 0xdf;
-        hashGenesisBlock = uint256("371d845655c9279a5e3c4a2dd43fbe02ffc8bb34c8bde65a33d818209c72022b");
+        hashGenesisBlock = uint256("oxo");
     }
 
     //
